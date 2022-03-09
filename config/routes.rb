860e6f87acddf13631e3b80c28ab54fbc5e506f3
Michael Cheung <michaelcheung.cheme@gmail.com>
2022-03-09 16:31:35 -0700

    backend working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get '/pokemons', to: 'pokemons#index'
+    post '/pokemons/', to: 'pokemons#create'
+    get '/pokemons/:id', to: 'pokemons#show'
+    delete '/pokemons/:id', to: 'pokemons#destroy'
+    put '/pokemons/:id', to: 'pokemons#update'
+    patch '/pokemons/:id', to: 'pokemons#update'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pokemon.destroy_all
+
+6.times do 
+  Pokemon.create(name:Faker::Games::Pokemon.name, location: Faker::Games::Pokemon.location)
+end
+
+p "====================================="
+p "# of pokemon = #{Pokemon.count}"
